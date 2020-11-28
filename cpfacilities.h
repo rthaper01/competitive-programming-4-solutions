329bd48f3584325a9de8659dcf01d7409dadb909
Rishubh Thaper <74514453+rthaper01@users.noreply.github.com>
2020-11-28 15:26:58 -0500

    Add files via upload

---
+++ cpfacilities.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <map>
+#include <string>
+#include <fstream>
+#include <cstring>
+
+typedef long long ll;
+typedef std::pair<int, int> ii;
