972af88aae1805baa2ef571ab13d29b805313fec
Rishubh Thaper <74514453+rthaper01@users.noreply.github.com>
2020-12-10 16:24:01 -0500

    Update chess.cpp

--- Chapter 1/Kattis/chess.cpp
+++ Chapter 1/Kattis/chess.cpp
@@ -9,10 +9,8 @@
 
 using namespace std;
 
-unordered_map<int, string> rows = {{0,"A"},{1,"B"},{2,"C"},{3,"D"},
-						 {4,"E"},{5,"F"},{6,"G"},{7,"H"}};
-unordered_map<char, int> rows_reverse = {{'A',0},{'B',1},{'C',2},{'D',3},
-											{'E',4},{'F',5},{'G',6},{'H',7}};
+const unordered_map<int, string> rows = {{0,"A"},{1,"B"},{2,"C"},{3,"D"},{4,"E"},{5,"F"},{6,"G"},{7,"H"}};
+const unordered_map<char, int> rows_reverse = {{'A',0},{'B',1},{'C',2},{'D',3},{'E',4},{'F',5},{'G',6},{'H',7}};
 
 bool in_bounds(pair<int, int> square) {
 	if (square.first <= 7 && square.second <= 7 && square.first >= 0 &&
